4527149a83cabd878df95beda96c45b55ac44754
brianlfarmerllc <brianlfarmerllc@gmail.com>
2021-05-09 21:29:10 -0500

    commit working page before coding dropdowns li's

--- scripts.js
+++ scripts.js
@@ -1,19 +1,32 @@
 // var
 const login = document.querySelector("#login")
 const links = document.querySelector("#intro-links")
+const navUl = document.querySelector("#main-ul")
 
 // functions
 function toggleActive(event) {
-    const target = event.target.className
+    const target = event.target
     const childList = event.target.parentElement.children
-    if (target != "active") {
+    const img = event.target.children[0]
+    if (target.className != "active" && target.localName === "button") {
         for (const button of childList) {
             button.classList.toggle("active")
         }
+    } else if (target.localName === "a") {
+        img.classList.toggle("active")
+    } else {
+
     }
 }
+
+// function flipArrow(event) {
+//     const img = event.target.children[0]
+//     console.log(img.className)
+//     img.classList.toggle("active")
+// }
 
 // events
 
 login.addEventListener("click", toggleActive);
 links.addEventListener("click", toggleActive);
+navUl.addEventListener("click", toggleActive);
