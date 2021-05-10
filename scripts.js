2145ece0da8d840b04bfcd211b30e2cd947ba854
brianlfarmerllc <brianlfarmerllc@gmail.com>
2021-05-10 03:15:39 -0500

    clean up js

--- scripts.js
+++ scripts.js
@@ -2,32 +2,29 @@
 const login = document.querySelector("#login")
 const links = document.querySelector("#intro-links")
 const navUl = document.querySelector("#main-ul")
+const inactiveUl = document.querySelectorAll("#dropUL-inactive")
+const arrow = document.querySelectorAll("#arrow")
 
 // functions
 function toggleActive(event) {
-    const target = event.target
-    const childList = event.target.parentElement.children
-    const img = event.target.children[0]
-    const hiddenUl = event.target.parentElement.children[1]
+    // function vars
+    const target = event.target;
+    const childList = event.target.parentElement.children;
+    const img = event.target.children[0];
+    const hiddenUl = event.target.parentElement.children[1];
+    // clear all active classes
+    inactiveUl.forEach(ul => ul.classList.remove("dropUl"));
+    arrow.forEach(img => img.classList.remove("active"));
+    // function for targets
     if (target.className != "active" && target.localName === "button") {
         for (const button of childList) {
-            button.classList.toggle("active")
+            button.classList.toggle("active");
         }
     } else if (target.localName === "a") {
         img.classList.toggle("active");
         hiddenUl.classList.toggle("dropUl");
-    } else {
-
     }
 }
-
-// function dropUl(event) {
-//     event.stop
-//     const hiddenUl = event.target.parentElement.children[1]
-//     const img = event.target.children[0]
-//     hiddenUl.classList.toggle("dropUl")
-//     img.classList.toggle("active")
-// }
 
 // events
 
