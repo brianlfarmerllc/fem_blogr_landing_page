a9810b530fbb42d0610b50e946847ec41be6e502
brianlfarmerllc <brianlfarmerllc@gmail.com>
2021-05-10 01:14:48 -0500

    creat script to toggle active states

--- scripts.js
+++ scripts.js
@@ -8,20 +8,24 @@ function toggleActive(event) {
     const target = event.target
     const childList = event.target.parentElement.children
     const img = event.target.children[0]
+    const hiddenUl = event.target.parentElement.children[1]
     if (target.className != "active" && target.localName === "button") {
         for (const button of childList) {
             button.classList.toggle("active")
         }
     } else if (target.localName === "a") {
-        img.classList.toggle("active")
+        img.classList.toggle("active");
+        hiddenUl.classList.toggle("dropUl");
     } else {
 
     }
 }
 
-// function flipArrow(event) {
+// function dropUl(event) {
+//     event.stop
+//     const hiddenUl = event.target.parentElement.children[1]
 //     const img = event.target.children[0]
-//     console.log(img.className)
+//     hiddenUl.classList.toggle("dropUl")
 //     img.classList.toggle("active")
 // }
 
